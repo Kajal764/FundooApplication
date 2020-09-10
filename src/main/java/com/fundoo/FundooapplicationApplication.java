77011163bf9de28f72124f6866dca9df388348a9
kajal764 <kajaldw666@gmail.com>
2020-09-10 06:04:37 -0700

    Added: Project package structure

---
+++ src/main/java/com/fundoo/FundooapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.fundoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundooapplicationApplication.class, args);
+    }
+
+}
