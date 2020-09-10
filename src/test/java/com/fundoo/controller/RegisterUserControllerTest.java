7707e6ddf3c095a95cc5430b15d034110ad03419
kajal764 <kajaldw666@gmail.com>
2020-09-10 12:57:29 -0700

    Refactored : Test Cases to follow DRY principle

--- src/test/java/com/fundoo/controller/RegisterUserControllerTest.java
+++ src/test/java/com/fundoo/controller/RegisterUserControllerTest.java
@@ -4,6 +4,7 @@ import com.fundoo.dto.RegisterUserDto;
 import com.fundoo.service.RegistrationService;
 import com.google.gson.Gson;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,12 +29,18 @@ public class RegisterUserControllerTest {
     RegistrationService registrationService;
 
     Gson gson = new Gson();
+    RegisterUserDto registerUserDto;
+    String toJson;
+
+    @BeforeEach
+    void setUp() {
+        registerUserDto = new RegisterUserDto("kajal", "waghmare", "kajalw1998@gmail.com", "1234");
+        toJson = gson.toJson(registerUserDto);
+        when(registrationService.register(any())).thenReturn("Registration Successfull");
+    }
 
     @Test
     void givenRequest_WhenGetResponse_ItShouldReturnStatusOk() throws Exception {
-        RegisterUserDto registerUserDto = new RegisterUserDto("kajal", "waghmare", "kajalw1998@gmail.com", "1234");
-        String toJson = gson.toJson(registerUserDto);
-        when(registrationService.register(any())).thenReturn("Registration Successfull");
         MvcResult mvcResult = this.mockMvc.perform(post("/fundoo/register").content(toJson)
                 .contentType(MediaType.APPLICATION_JSON)).andReturn();
         Assert.assertEquals(mvcResult.getResponse().getStatus(), 200);
@@ -41,9 +48,6 @@ public class RegisterUserControllerTest {
 
     @Test
     void givenRequest_WhenGetResponse_ItShouldReturnSuccessMessage() throws Exception {
-        RegisterUserDto registerUserDto = new RegisterUserDto("kajal", "waghmare", "kajalw1998@gmail.com", "1234");
-        String toJson = gson.toJson(registerUserDto);
-        when(registrationService.register(any())).thenReturn("Registration Successfull");
         MvcResult mvcResult = this.mockMvc.perform(post("/fundoo/register").content(toJson)
                 .contentType(MediaType.APPLICATION_JSON)).andReturn();
         Assert.assertEquals(mvcResult.getResponse().getContentAsString(), "Registration Successfull");
@@ -51,9 +55,6 @@ public class RegisterUserControllerTest {
 
     @Test
     void givenRequestWithAnotherMediaType_WhenGetResponse_ItShouldNotAccept() throws Exception {
-        RegisterUserDto registerUserDto = new RegisterUserDto("kajal", "waghmare", "kajalw1998@gmail.com", "1234");
-        String toJson = gson.toJson(registerUserDto);
-        when(registrationService.register(any())).thenReturn("Registration Successfull");
         MvcResult mvcResult = this.mockMvc.perform(post("/fundoo/register").content(toJson)
                 .contentType(MediaType.APPLICATION_PROBLEM_XML_VALUE)).andReturn();
         Assert.assertEquals(mvcResult.getResponse().getContentAsString(), "Unsupported Media Type");
