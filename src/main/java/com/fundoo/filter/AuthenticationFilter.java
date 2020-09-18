75a68fcba89cde18255ca2b85813cefaae85a8e9
kajal764 <kajaldw666@gmail.com>
2020-09-18 04:21:55 -0700

    Added: Exception for filter if token not provided

--- src/main/java/com/fundoo/filter/AuthenticationFilter.java
+++ src/main/java/com/fundoo/filter/AuthenticationFilter.java
@@ -6,6 +6,7 @@ import com.fundoo.user.service.RedisService;
 import com.fundoo.user.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -13,20 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebFilter(urlPatterns = {"/fundoo/note/*","/fundoo/label/*"})
+@WebFilter(urlPatterns = {"/fundoo/note/*", "/fundoo/label/*"})
 public class AuthenticationFilter implements Filter {
 
     @Autowired
     UserRepository userRepository;
 
     @Autowired
-    JwtUtil jwtUtil=new JwtUtil();
+    JwtUtil jwtUtil = new JwtUtil();
 
     @Autowired
     HandlerExceptionResolver handlerExceptionResolver;
 
     @Autowired
-    RedisService redisService =new RedisService();
+    RedisService redisService = new RedisService();
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -35,24 +36,23 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         String authorizationHeader = request.getHeader("Authorization");
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String jwtToken = authorizationHeader.substring(7);
-            Object email = jwtUtil.verify(jwtToken);
-            String token = redisService.getToken(email.toString());
-            try{
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                String jwtToken = authorizationHeader.substring(7);
+                Object email = jwtUtil.verify(jwtToken);
+                String token = redisService.getToken(email.toString());
                 if (jwtToken.equals(token)) {
                     servletRequest.setAttribute("email", email);
                     filterChain.doFilter(request, response);
                 } else {
-                    throw new AuthenticationException("User Don't have permission");
+                    throw new AuthenticationException("Authorization fail");
                 }
-            }catch (AuthenticationException exception){
-                handlerExceptionResolver.resolveException(request,response,null,exception);
+            } else {
+                throw new AuthenticationException("User Don't have permission");
             }
+        } catch (AuthenticationException exception) {
+            handlerExceptionResolver.resolveException(request, response, null, exception);
         }
     }
-
-
 
 }
