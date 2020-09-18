057752a9d3ac711d6a835c107bfdcceb28b323fc
kajal764 <kajaldw666@gmail.com>
2020-09-18 00:32:11 -0700

    Added: Exception if authentication fail

--- src/main/java/com/fundoo/filter/AuthenticationFilter.java
+++ src/main/java/com/fundoo/filter/AuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.fundoo.filter;
 
+import com.fundoo.note.exception.AuthenticationException;
 import com.fundoo.user.repository.UserRepository;
 import com.fundoo.user.service.RedisService;
 import com.fundoo.user.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +24,9 @@ public class AuthenticationFilter implements Filter {
     JwtUtil jwtUtil=new JwtUtil();
 
     @Autowired
+    HandlerExceptionResolver handlerExceptionResolver;
+
+    @Autowired
     RedisService redisService =new RedisService();
 
     @Override
@@ -35,14 +41,17 @@ public class AuthenticationFilter implements Filter {
             String jwtToken = authorizationHeader.substring(7);
             Object email = jwtUtil.verify(jwtToken);
             String token = redisService.getToken(email.toString());
-            if (jwtToken.equals(token)) {
-                servletRequest.setAttribute("email", email);
-                filterChain.doFilter(request, response);
-            } else {
-                PrintWriter out = response.getWriter();
-                out.println("User Don't have permission");
-                //throw new AuthenticationException("User Don't have permission");
+            try{
+                if (jwtToken.equals(token)) {
+                    servletRequest.setAttribute("email", email);
+                    filterChain.doFilter(request, response);
+                } else {
+                    throw new AuthenticationException("User Don't have permission");
+                }
+            }catch (AuthenticationException exception){
+                handlerExceptionResolver.resolveException(request,response,null,exception);
             }
+
         }
     }
 
