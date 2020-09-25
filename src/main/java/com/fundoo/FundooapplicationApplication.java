13e4cfb432d212d9fc0630feab86b8c1f72779e9
kajal764 <kajaldw666@gmail.com>
2020-09-25 11:48:04 -0700

    Added: Swagger for api

--- src/main/java/com/fundoo/FundooapplicationApplication.java
+++ src/main/java/com/fundoo/FundooapplicationApplication.java
@@ -3,11 +3,9 @@ package com.fundoo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ServletComponentScan
-@EnableJpaRepositories
 public class FundooapplicationApplication {
     public static void main(String[] args) {
         SpringApplication.run(FundooapplicationApplication.class, args);
