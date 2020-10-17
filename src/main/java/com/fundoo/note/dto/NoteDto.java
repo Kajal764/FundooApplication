7d2a6a1b9fb0c23ae664938027b1ea03efcc8bcc
kajal764 <kajaldw666@gmail.com>
2020-10-17 10:35:30 +0530

    [Kajal]: Modified Note fields validation

--- src/main/java/com/fundoo/note/dto/NoteDto.java
+++ src/main/java/com/fundoo/note/dto/NoteDto.java
@@ -1,6 +1,8 @@
 package com.fundoo.note.dto;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
@@ -12,10 +14,12 @@ public class NoteDto {
 
     public int note_id;
 
-    @NotNull(message = "Title must not be null")
+    @NotNull
+    @Length(min = 1, message = "Title must not be null")
     public String title;
 
-    @NotNull(message = "Description must not be null")
+    @NotNull
+    @Length(min = 1, message = "Description must not be null")
     public String description;
 
 }
--- src/main/java/com/fundoo/note/model/Note.java
+++ src/main/java/com/fundoo/note/model/Note.java
@@ -1,6 +1,7 @@
 package com.fundoo.note.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fundoo.label.model.Label;
 import com.fundoo.user.model.User;
 import lombok.*;
@@ -21,29 +22,40 @@ public class Note {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer note_Id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String description;
 
+    @JsonIgnore
     private LocalDateTime createdDate = LocalDateTime.now();
 
+    @JsonIgnore
     private LocalDateTime editDate;
 
+    @JsonIgnore
     private boolean isTrash;
 
+    @JsonIgnore
     private boolean isPin;
 
+    @JsonIgnore
     private boolean isArchive;
 
+    @JsonIgnore
     private boolean isCollaborateNote;
 
+    @JsonIgnore
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm a")
     private LocalDateTime remainder;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "noteList", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<User> userList;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "noteList", cascade = CascadeType.REMOVE)
     private List<Label> labelList;
 
